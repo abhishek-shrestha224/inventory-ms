58d219cef9220535ea649a74dc378f5bb5252d58
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-16 20:57:56 +0545

    ...

---
+++ src/main/resources/db/migration/V2__seed.sql
@@ -0,0 +1,12 @@
+-- Insert seed data into t_inventory
+INSERT INTO `t_inventory` (`sku_code`, `quantity`) VALUES
+('SKU001', 100),
+('SKU002', 50),
+('SKU003', 200),
+('SKU004', 10),
+('SKU005', 75),
+('SKU006', 150),
+('SKU007', 90),
+('SKU008', 120),
+('SKU009', 60),
+('SKU010', 110);
