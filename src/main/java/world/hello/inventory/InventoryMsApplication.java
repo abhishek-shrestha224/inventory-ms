b1d36892e7f56395abf855dc4a91954c0ea21d6d
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-16 19:04:21 +0545

    init

---
+++ src/main/java/world/hello/inventory/InventoryMsApplication.java
@@ -0,0 +1,12 @@
+package world.hello.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InventoryMsApplication.class, args);
+  }
+}
---
+++ src/test/java/world/hello/inventory/InventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package world.hello.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
